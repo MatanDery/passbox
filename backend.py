d0c7fb573aa8ec43a19d392e8dfe276a123c4d79
Matan Dery <matandery1997@gmail.com>
2020-05-04 13:24:09 +0300

    more functions backend

--- backend.py
+++ backend.py
@@ -1,18 +1,52 @@
 import sqlite3
 
+def create_table():
+    con = sqlite3.connect('creds.db')
+    cur = con.cursor()
+    cur.execute('CREATE TABLE users(site, name, passwd)')
+    con.commit()
+    con.close()
 
-con = sqlite3.connect('creds.db')
-cur = con.cursor()
-#cur.execute('CREATE TABLE users(name, site, passwd)')
+def add_entry_to_table(site,user,passwd):
+    con = sqlite3.connect('creds.db')
+    cur = con.cursor()
+    cur.execute("INSERT INTO users values(?, ?, ?)", (site, user, passwd))
+    con.commit()
+    con.close()
+
+def view_table():
+    con = sqlite3.connect('creds.db')
+    cur = con.cursor()
+    print(cur.execute("SELECT * FROM users ").fetchall())
+    con.close()
+
+def view_entry(site):
+    con = sqlite3.connect('creds.db')
+    cur = con.cursor()
+    print(cur.execute('SELECT * FROM users Where site=?', (site, )).fetchone())
+    con.close()
+
+def delete_entry(site):
+    con = sqlite3.connect('creds.db')
+    cur = con.cursor()
+    print(cur.execute("DELETE * FROM users where site =(?)",(site)))
+    con.commit()
+    con.close()
+
+
+# con = sqlite3.connect('creds.db')
+# cur = con.cursor()
 user='matan543'
 site='alibaba'
 passwd='P@ssw0rd'
-cur.execute("INSERT INTO users values(?, ?, ?)",(user, site, passwd))
-print(cur.execute("SELECT * FROM users ").fetchall())
-#con.commit()
-con.close()
-
-
-
+# print(cur.execute("SELECT * FROM users ").fetchall())
+# con.commit()
+# con.close()
+# create_table()
+# add_entry_to_table(site,user,passwd)
+# add_entry_to_table(site+'123', user, passwd)
+# add_entry_to_table(site+'456', user, passwd)
+view_table()
+view_entry('alibaba123')
 
 
