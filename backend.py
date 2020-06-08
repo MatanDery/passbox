b67e3a4e2f40fb4872158961e8c030b924359004
Matan Dery <matandery1997@gmail.com>
2020-06-08 14:35:48 +0300

    Added gui

--- backend.py
+++ backend.py
@@ -1,44 +1,64 @@
 import sqlite3
+from hashlib import sha256         # can be used with sha-1 https://haveibeenpwned.com/API/v3
+from binascii import hexlify
 
-def create_table():
-    con = sqlite3.connect('creds.db')
-    cur = con.cursor()
-    cur.execute('CREATE TABLE users(site, name, passwd)')
-    con.commit()
-    con.close()
+class Back:
 
-def add_entry_to_table(site,user,passwd):
-    con = sqlite3.connect('creds.db')
-    cur = con.cursor()
-    cur.execute("INSERT INTO users values(?, ?, ?)", (site, user, passwd))
-    con.commit()
-    con.close()
+    def __init__(self):
+        self.con = sqlite3.connect('creds.db')
+        self.cur = self.con.cursor()
+        self.cur.execute('CREATE TABLE IF NOT EXISTS users(site, name, passwd)')
+        self.con.commit()
 
-def view_table():
-    con = sqlite3.connect('creds.db')
-    cur = con.cursor()
-    print(cur.execute("SELECT * FROM users ").fetchall())
-    con.close()
+    def add_entry_to_table(self, site, user, passwd):
+        salt = site
+        pepper = 'qwerty'
+        passwd = salt + passwd + pepper
+        passwd = hexlify(sha256(passwd.encode()).digest())
+        x = self.cur.execute("SELECT * FROM users where site=(?) AND name=(?) AND passwd=(?)", (site, user, passwd))
+        if len(x.fetchall()) > 0 :
+            return 'ALREADY EXISTS'
+        else:
+            self.cur.execute("INSERT INTO users values(?, ?, ?)", (site, user, passwd))
+            self.con.commit()
 
-def view_entry(site):
-    con = sqlite3.connect('creds.db')
-    cur = con.cursor()
-    print(cur.execute('SELECT * FROM users Where site=?', (site, )).fetchone())
-    con.close()
+    def view_table(self):
+        return (self.cur.execute("SELECT * FROM users ").fetchall())
 
-def delete_entry(site):
-    con = sqlite3.connect('creds.db')
-    cur = con.cursor()
-    print(cur.execute("DELETE * FROM users where site =(?)",(site)))
-    con.commit()
-    con.close()
+    def view_entry(self, site):
+        return self.cur.execute('SELECT * FROM users Where site=(?)', (site, )).fetchone()
+
+    def delete_entry(self, site):
+        self.cur.execute("DELETE FROM users where site =?", (site, ))
+        self.con.commit()
+
+    def edit_entry_site(self, site, new_site):
+        self.cur.execute("UPDATE users SET site =(?) WHERE site =(?)", (new_site, site))
+        self.con.commit()
+
+    def search_by_pass(self, passwd):
+        table = self.view_table()
+        exists = []
+        for i in table:
+            site, user, curpasswd = i[0], i[1], i[2]
+            salt = site
+            pepper = 'qwerty'
+            check_passwd = salt + passwd + pepper
+            check_passwd = hexlify(sha256(check_passwd.encode()).digest())
+            if check_passwd == curpasswd:
+                exists.append(i)
+        return exists
+
+    def quit_db(self):
+        self.con.close()
+
 
 
 # con = sqlite3.connect('creds.db')
 # cur = con.cursor()
-user='matan543'
-site='alibaba'
-passwd='P@ssw0rd'
+# user='matan543'
+# site='alibaba'
+# passwd='P@ssw0rd'
 # print(cur.execute("SELECT * FROM users ").fetchall())
 # con.commit()
 # con.close()
@@ -46,7 +66,7 @@ passwd='P@ssw0rd'
 # add_entry_to_table(site,user,passwd)
 # add_entry_to_table(site+'123', user, passwd)
 # add_entry_to_table(site+'456', user, passwd)
-view_table()
-view_entry('alibaba123')
-
+# view_table()
+# view_entry('alibaba123')
+# edit_entry_site("alibaba123","google.com")
 
---
+++ frontend.py
@@ -0,0 +1,155 @@
+from tkinter import *
+from tkinter import ttk
+from backend import Back
+from Crypto.Cipher import AES
+
+class popup_dec():
+    def __init__(self):
+        try:
+            with open('creds.db', 'rb') as f:
+                if f.read()[:15] == b'SQLite format 3':
+                    return
+        except:
+            return
+        self.popup = Tk()
+        self.popup.wm_title("!")
+        label = ttk.Label(self.popup, text="ENTER PASSWORD!")
+        lable_user = Label(text="Enter Decription Pass")
+        lable_user.pack()
+        self.pass_dec = StringVar()
+        self.entry_pass = Entry(textvariable=self.pass_dec)
+        self.entry_pass.pack()
+        b1 = ttk.Button(self.popup, text="Okay", command = self.dec)
+        b1.pack()
+        self.popup.mainloop()
+
+    def dec(self):
+        key = self.pass_dec.get()
+        if len(key) % 16 != 0:
+            padding = 16 - len(key) % 16
+            key = key + 'A' * padding
+        key = key.encode()
+        with open('creds.db', 'rb') as f:
+            data = f.read()
+            if data[:15] == b'SQLite format 3':
+                return
+            else:
+                f.close()
+                with open('creds.db', 'wb') as f:
+                    enc_data = AES.new(key, AES.MODE_ECB)
+                    enc_data = enc_data.decrypt(data)
+                    print(enc_data)
+                    if enc_data[:15] == b'SQLite format 3':
+                        f.write(enc_data)
+                        self.popup.destroy()
+                    else:
+                        f.write(data)
+                        return
+
+
+
+
+popup_dec()
+
+back_db =Back()
+
+class Ui():
+    def __init__(self, main_window):
+        main_window.wm_title("Users Locker")
+        lable_site = Label(text="site")
+        lable_site.grid(row=0, column=0)
+        self.entry_site_val=StringVar()
+        self.entry_site = Entry(textvariable=self.entry_site_val)
+        self.entry_site.grid(row=0, column=1)
+
+        lable_user = Label(text="username")
+        lable_user.grid(row=1, column=0)
+        self.entry_user_val=StringVar()
+        self.entry_user = Entry(textvariable=self.entry_user_val)
+        self.entry_user.grid(row=1, column=1)
+
+        lable_pass = Label(text="password")
+        lable_pass.grid(row=2, column=0)
+        self.entry_pass_val=StringVar()
+        self.entry_pass = Entry(textvariable=self.entry_pass_val)
+        self.entry_pass.grid(row=2, column=1)
+
+        self.cred_list = Listbox(main_window, height=30, width=80, )
+        self.cred_list.grid(row=5, column=0, rowspan=10, columnspan=4)
+
+
+        view_all_button = Button(main_window, text='View All', command=self.view_all_wrap)
+        view_all_button.grid(row=4, column=0)
+
+        add_entry_button = Button(main_window, text='Add Site', command=self.add_entry_wrap)
+        add_entry_button.grid(row=4, column=1)
+
+        del_entry_button = Button(main_window, text='Delete Site', command=self.del_entry_wrap)
+        del_entry_button.grid(row=4, column=2)
+
+        search_entry_button = Button(main_window, text='Search Site', command=self.find_site)
+        search_entry_button.grid(row=4, column=3)
+
+        self.enc_pass = StringVar()
+        self.enc_pass = Entry(textvariable=self.enc_pass)
+        self.enc_pass.grid(row=3, column=10)
+        enc_button = Button(main_window, text='Encrypt DB', command=self.enc)
+        enc_button.grid(row=4, column=10)
+
+        enc_button = Button(main_window, text='Search Password', command=self.find_passwd)
+        enc_button.grid(row=4, column=4)
+
+
+    def enc(self):
+        key = self.enc_pass.get()
+        if len(key) == 0:
+            return
+        if len(key) % 16 != 0:
+            padding = 16 - len(key) % 16
+            key = key + 'A' * padding
+        key = key.encode()
+        with open('creds.db', 'rb') as f:
+            data = f.read()
+            if data[:15] != b'SQLite format 3':
+                return
+            else:
+                f.close()
+                with open('creds.db', 'wb') as f:
+                    enc_data = AES.new(key, AES.MODE_ECB)
+                    enc_data = enc_data.encrypt(data)
+                    f.write(enc_data)
+                    quit()
+
+    def view_all_wrap(self):
+        self.cred_list.delete(0,END)
+        for row in back_db.view_table():
+            self.cred_list.insert(END, row)
+
+    def add_entry_wrap(self):
+        back_db.add_entry_to_table(self.entry_site_val.get(), self.entry_user_val.get(), self.entry_pass_val.get())
+        self.view_all_wrap()
+
+    def del_entry_wrap(self):
+        back_db.delete_entry(self.entry_site_val.get())
+        self.view_all_wrap()
+
+    def find_site(self):
+        res = back_db.view_entry(self.entry_site_val.get())
+        self.cred_list.delete(0, END)
+        self.cred_list.insert(END,res)
+
+    def find_passwd(self):
+        exists = back_db.search_by_pass(self.entry_pass_val.get())
+        self.cred_list.delete(0, END)
+        for row in exists:
+            self.cred_list.insert(END, row)
+
+
+
+main_window = Tk()
+Ui(main_window)
+main_window.mainloop()
+
+
+
+
